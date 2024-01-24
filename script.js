bb4dd6c2da6e01e41ded35f1ccdc9793108133c3
ArjunB126 <arjunbajare86@gmail.com>
2024-01-24 18:30:39 +0530

    Js_Updates

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
